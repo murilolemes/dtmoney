a1cd714e31f12903bc152da33b82ccdfc93f8f51
Murilo Lemes <murilo.lemes.mhl@gmail.com>
2022-02-14 17:03:48 -0300

    melhorias no cabeçalho e modal de transações

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,53 @@
+import Modal from 'react-modal';
+
+import CloseImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container, TransactionTypeContainer } from './styles';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    >
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-close'
+      >
+        <img src={CloseImg} alt="Fechar Modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input placeholder='Titulo' />
+        <input type='number' placeholder='Valor' />
+        <TransactionTypeContainer>
+          <button
+            type='button'
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button
+            type='button'
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+        <input placeholder='Categoria' />
+        <button type='submit'>Cadastrar</button>
+      </Container>
+    </Modal>
+  )
+}
